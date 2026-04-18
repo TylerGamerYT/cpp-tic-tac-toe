9d08e03e3549354015690d056d8de65b57921cdf
Tyler <lewis22511@hotmail.com>
2026-04-18 19:29:41 +0100

    Update game.h

--- src/game.h
+++ src/game.h
@@ -3,20 +3,25 @@
 
 class Game {
 public:
-    void run();
+    void menu();
 
 private:
     char board[3][3];
     char player;
     int scoreX, scoreO;
 
+    void startPvP();
+    void startAI();
+
     void initBoard();
     void drawBoard();
     bool checkWin();
     bool checkDraw();
     void inputMove();
+
     void saveScore();
     void loadScore();
+    void resetBoard();
 };
 
 #endif
