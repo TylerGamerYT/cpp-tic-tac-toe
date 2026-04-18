7b7dd9b16084d7fa9568e0959c575f393886f9ee
Tyler <lewis22511@hotmail.com>
2026-04-18 19:26:29 +0100

    Update game.h

--- src/game.h
+++ src/game.h
@@ -1,1 +1,22 @@
+#ifndef GAME_H
+#define GAME_H
 
+class Game {
+public:
+    void run();
+
+private:
+    char board[3][3];
+    char player;
+    int scoreX, scoreO;
+
+    void initBoard();
+    void drawBoard();
+    bool checkWin();
+    bool checkDraw();
+    void inputMove();
+    void saveScore();
+    void loadScore();
+};
+
+#endif
