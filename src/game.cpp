2ab21e59c54edd103b982a77b169795816ef8378
Tyler <lewis22511@hotmail.com>
2026-04-18 19:30:02 +0100

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 #include <fstream>
 #include "game.h"
+#include "ai.h"
 
 using namespace std;
 
+void Game::menu() {
+    loadScore();
+
+    int choice;
+
+    while (true) {
+        cout << "\n=== TIC TAC TOE ===\n";
+        cout << "1. Player vs Player\n";
+        cout << "2. Player vs AI\n";
+        cout << "3. Exit\n";
+        cout << "Choice: ";
+        cin >> choice;
+
+        if (choice == 1) startPvP();
+        else if (choice == 2) startAI();
+        else break;
+    }
+}
+
 void Game::initBoard() {
     char c = '1';
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            board[i][j] = c++;
+    for (auto &row : board)
+        for (char &cell : row)
+            cell = c++;
+
     player = 'X';
 }
 
+void Game::resetBoard() {
+    initBoard();
+}
+
 void Game::drawBoard() {
-    system("clear"); // use "cls" on Windows
+    system("clear"); // cls for Windows
 
     cout << "\nTIC TAC TOE\n\n";
 
@@ -22,24 +47,24 @@ void Game::drawBoard() {
         if (i < 2) cout << "---|---|---\n";
     }
 
-    cout << "\nScore X: " << scoreX << " | Score O: " << scoreO << "\n\n";
+    cout << "\nScore X: " << scoreX << " | Score O: " << scoreO << "\n";
 }
 
 bool Game::checkWin() {
     for (int i = 0; i < 3; i++) {
-        if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) return true;
-        if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) return true;
+        if (board[i][0]==board[i][1] && board[i][1]==board[i][2]) return true;
+        if (board[0][i]==board[1][i] && board[1][i]==board[2][i]) return true;
     }
 
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) return true;
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) return true;
+    if (board[0][0]==board[1][1] && board[1][1]==board[2][2]) return true;
+    if (board[0][2]==board[1][1] && board[1][1]==board[2][0]) return true;
 
     return false;
 }
 
 bool Game::checkDraw() {
-    for (auto &row : board)
-        for (char c : row)
+    for (auto &r : board)
+        for (char c : r)
             if (c != 'X' && c != 'O')
                 return false;
     return true;
@@ -50,15 +75,10 @@ void Game::inputMove() {
     cout << "Player " << player << " move (1-9): ";
     cin >> move;
 
-    if (move < 1 || move > 9) return;
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == move + '0') {
-                board[i][j] = player;
-            }
-        }
-    }
+    for (auto &row : board)
+        for (char &cell : row)
+            if (cell == move + '0')
+                cell = player;
 
     player = (player == 'X') ? 'O' : 'X';
 }
@@ -66,7 +86,6 @@ void Game::inputMove() {
 void Game::saveScore() {
     ofstream file("data/scores.txt");
     file << scoreX << " " << scoreO;
-    file.close();
 }
 
 void Game::loadScore() {
@@ -74,9 +93,8 @@ void Game::loadScore() {
     if (file) file >> scoreX >> scoreO;
 }
 
-void Game::run() {
-    loadScore();
-    initBoard();
+void Game::startPvP() {
+    resetBoard();
 
     while (true) {
         drawBoard();
@@ -84,11 +102,38 @@ void Game::run() {
 
         if (checkWin()) {
             drawBoard();
-            cout << "Winner: " << (player == 'X' ? 'O' : 'X') << endl;
+            cout << "Winner: " << (player=='X'?'O':'X') << "\n";
+            if (player=='X') scoreO++; else scoreX++;
+            saveScore();
+            break;
+        }
 
-            if (player == 'X') scoreO++;
-            else scoreX++;
+        if (checkDraw()) {
+            drawBoard();
+            cout << "Draw!\n";
+            break;
+        }
+    }
+}
 
+void Game::startAI() {
+    resetBoard();
+    AI ai;
+
+    while (true) {
+        drawBoard();
+
+        if (player == 'X') {
+            inputMove();
+        } else {
+            ai.bestMove(board, player);
+            player = 'X';
+        }
+
+        if (checkWin()) {
+            drawBoard();
+            cout << "Winner: " << (player=='X'?'O':'X') << "\n";
+            if (player=='X') scoreO++; else scoreX++;
             saveScore();
             break;
         }
